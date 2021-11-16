638ccb5d125581e067e3d0a68cfc3022812d3060
Aroa <aroa.guirado@gmail.com>
2021-11-16 19:58:02 +0100

    Añadimos comentario con nombre

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // Aroa Guirado Picazo
         System.out.println("Estudiante " + args[0] + " : ");
         System.out.println(veredicto(args[1],args[2]));
     }
